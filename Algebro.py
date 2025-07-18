2b80b4a0edc234ac1986ec3ca2ec6716a89cc634
Ayaan <93127341+LaborotoryCode@users.noreply.github.com>
2025-07-18 22:54:01 +0800

    Non-cli Algebro

--- Algebro.py
+++ Algebro.py
@@ -3,103 +3,78 @@ from pylatexenc.latex2text import LatexNodes2Text
 import os
 import re
 
-#model config
 model = SentenceTransformer("all-MiniLM-L6-v2")
 
-
-def convert_latex_question(latex_str):
-    # Clean using pylatexenc
-    text = LatexNodes2Text().latex_to_text(latex_str)
-
-    # Extract math parts
-    math_parts = re.findall(r'∫.*?dx', text)
-    
-    for mp in math_parts:
-        # Replace inline math with a block-style rendering
-        formatted = f"\n{mp}\n"
-        text = text.replace(mp, formatted)
-    
-    return text.strip()
-
-category = int(input("""
-1. Functions and graphs
-2. Sequences and series
-3. Vectors
-4. Intro to complex numbers
-5. Calculus
-6. Probability and Stats
-7. Random
-                     
-Enter the number of the topic you would like to practice: """))
-
-cat = {1: "Functions_and_graphs", 2: "Sequences_and_series", 3: "Vectors", 4: "Intro_to_complex_numbers", 5: "Calculus", 6: "Probability_and_Stats", 7: "Random"}
-
-questions_path = os.listdir(cat[category])
-
-questions = []
-
-for x in questions_path:
-    with open(f"{cat[category]}/{x}", "r") as f:
-        questions.append(f.read())
+cat = {
+    1: "Functions_and_graphs",
+    2: "Sequences_and_series",
+    3: "Vectors",
+    4: "Intro_to_complex_numbers",
+    5: "Calculus",
+    6: "Probability_and_Stats",
+    7: "Random"
+}
 
 beginning = r"""
-    %% LyX 2.3.6.1 created this file.  For more info, see http://www.lyx.org/.
-    %% Do not edit unless you really know what you are doing.
-    \documentclass[english]{article}
-    \usepackage[T1]{fontenc}
-    \usepackage[latin9]{inputenc}
-    \usepackage{geometry}
-    \geometry{verbose,tmargin=2.5cm,bmargin=2.5cm,lmargin=2.5cm,rmargin=2.5cm}
-    \usepackage{color}
-    \usepackage{setspace}
-    \PassOptionsToPackage{normalem}{ulem}
-    \usepackage{ulem}
+%% LyX 2.3.6.1 created this file. For more info, see http://www.lyx.org/.
+%% Do not edit unless you really know what you are doing.
+\documentclass[english]{article}
+\usepackage[T1]{fontenc}
+\usepackage[latin9]{inputenc}
+\usepackage{geometry}
+\geometry{verbose,tmargin=2.5cm,bmargin=2.5cm,lmargin=2.5cm,rmargin=2.5cm}
+\usepackage{color}
+\usepackage{setspace}
+\PassOptionsToPackage{normalem}{ulem}
+\usepackage{ulem}
 
-    \makeatletter
+\makeatletter
 
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%% LyX specific LaTeX commands.
-    %% Because html converters don't know tabularnewline
-    \providecommand{\tabularnewline}{\\}
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%% LyX specific LaTeX commands.
+%% Because html converters don't know tabularnewline
+\providecommand{\tabularnewline}{\\}
 
-    \makeatother
+\makeatother
 
-    \usepackage{babel}
-    \begin{document}
-    """
+\usepackage{babel}
+\begin{document}
+"""
 
 end = r"""\end{document}"""
-questions = list(map(lambda x: beginning+x+end, questions))
 
-others = []
+def convert_latex_question(latex_str: str) -> str:
+    # Convert LaTeX to plain text and format integrals nicely
+    text = LatexNodes2Text().latex_to_text(latex_str)
+    math_parts = re.findall(r'∫.*?dx', text)
+    for mp in math_parts:
+        text = text.replace(mp, f"\n{mp}\n")
+    return text.strip()
 
-question = LatexNodes2Text().latex_to_text(questions[0])
-print(question)
-question_embedding = model.encode(question)
+def load_questions(category_id: int) -> list[str]:
+    folder = cat.get(category_id)
+    if not folder:
+        raise ValueError(f"Invalid category ID: {category_id}")
+    questions = []
+    for filename in os.listdir(folder):
+        with open(os.path.join(folder, filename), "r") as f:
+            raw = f.read()
+            full_latex = beginning + raw + end
+            questions.append(full_latex)
+    return questions
 
-print(len(questions))
-questions.pop(0)
-print(len(questions))
+def get_question_text_and_embedding(question_latex: str):
+    text = convert_latex_question(question_latex)
+    embedding = model.encode(text)
+    return text, embedding
 
-while len(questions)>0:
-    print("length", len(questions))
+def select_next_question(current_embedding, questions: list[str], is_difficult: bool) -> str:
     comparisons = []
-    for i in questions:
-        similarity = model.similarity(model.encode(i), question_embedding)
-        comparisons.append([round(similarity.item(), 8), i])
-
-    wrong = input("Enter W if you found this question to be difficult: ")
-
-    if wrong.upper() == "W":
-        question = max(comparisons)
-        question = question[1]
-        index = questions.index(question)
-        question_embedding = model.encode(question)
-        questions.pop(index)
-        print(LatexNodes2Text().latex_to_text(question))
+    for q in questions:
+        emb = model.encode(convert_latex_question(q))
+        sim = model.similarity(emb, current_embedding)
+        comparisons.append((sim.item(), q))
+    if is_difficult:
+        next_question = max(comparisons, key=lambda x: x[0])[1]
     else:
-        question = min(comparisons)
-        question = question[1]
-        index = questions.index(question)
-        question_embedding = model.encode(question)
-        questions.pop(index)
-        print(LatexNodes2Text().latex_to_text(question))
+        next_question = min(comparisons, key=lambda x: x[0])[1]
+    return next_question
