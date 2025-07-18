649effd3f94d49ee5976b2ada819c9fc9f80a6b3
heiicha <joanlo682@gmail.com>
2025-07-18 23:32:42 +0800

    question work yay

--- backend.py
+++ backend.py
@@ -10,7 +10,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3001"],  # React frontend origin
+    allow_origins=["http://localhost:3000", "http://localhost:3001"],  # React frontend origins
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
